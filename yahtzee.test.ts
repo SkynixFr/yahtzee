814399b75deddb1d3b1b5c50c092f9b336cf8025
Ricky Khao <khaoricky@gmail.com>
2023-02-28 15:15:58 +0100

    feat(yahtzee.test): ajout test des valeurs 1

--- yahtzee.test.ts
+++ yahtzee.test.ts
@@ -1,1 +1,17 @@
-console.log("Hello test");
+import { describe, expect, test } from '@jest/globals';
+import Yahtzee from './yahtzee';
+
+describe('GetPointsFromRoll', () => {
+	test('Vérification 1 = 1*nombre de dé =1', () => {
+		//Arrange
+		const dice = [1, 1, 1, 2, 3];
+		const category = 'Ones';
+		const expectedScore = 3;
+
+		//Act
+		const actualScore = Yahtzee.calculateScore(dice, category);
+
+		//Assert
+		expect(actualScore).toEqual(expectedScore);
+	});
+});
